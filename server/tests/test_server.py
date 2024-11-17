533e81cde4bc5968f6d0869c1eb5b80032b34717
Tobias Ritter <tobias.ritter98@gmail.com>
2024-11-17 17:52:24 +0100

    update

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -24,3 +24,9 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Server is running."
+
+
+def test_reset():
+    response = client.get("/reset?token=abc")
+    assert response.status_code == 200
+    assert response.json() == "Database reset."
