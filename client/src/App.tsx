0a8d3c1df5b602926035db1f5c6762d578749656
Tobias Ritter <tobias.ritter98@gmail.com>
2024-12-01 15:35:57 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 
 type State = {
   data: string;
+  count: number;
 }
 
-const initialState: State = { data: "" };
+const initialState: State = { data: "", count: 0 };
 
 async function fetchApi(state: State): Promise<State> {
   return await fetch('/api').then(res => res.json()).then(data => {
@@ -13,14 +14,20 @@ async function fetchApi(state: State): Promise<State> {
   });
 }
 
+function increment(state: State): State {
+  return { ...state, count: state.count + 1 };
+}
+
 function App() {
   const [state, setState] = useState<State>(initialState);
 
-  fetchApi(state).then(newState => { setState(newState) });
+  // fetchApi(state).then(newState => { setState(newState) });
 
   return (
     <div className="App">
       {state.data ? <h1>{state.data}</h1> : <h1>Loading...</h1>}
+      {state.count}
+      <button onClick={() => setState(increment)}>Increment</button>
     </div >
   );
 }
