ac0d0389ee4d8907afa230791fbc737d0304d368
Tobias Ritter <tobias.ritter98@gmail.com>
2024-12-01 20:13:30 +0100

    add test

--- client/src/State.test.ts
+++ client/src/State.test.ts
@@ -1,7 +1,13 @@
-import { increment } from "./State";
+import { increment, updateData } from "./State";
 
 test("properly increments count", () => {
     const initialState = { data: "", count: 0 };
     const state = increment(initialState);
     expect(state.count).toBe(1);
+});
+
+test("properly updates data", () => {
+    const initialState = { data: "", count: 0 };
+    const state = updateData(initialState, "new data");
+    expect(state.data).toBe("new data");
 });
